2374f39b274afc450279b102297ca3b26888860a
Almira Dyan Gtierrez <gutierrez.almiradyan@gmail.com>
2025-06-10 18:30:55 +0800

    feat: Hero section entrance animation (header, subheading, body, button) and scroll-trigerred background transition

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import Image from "next/image";
 import ButtonLink from "./components/ButtonLink";
 import TextSplitter from "./components/TextSplitter";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default function Home() {
   useGSAP(() => {
@@ -36,6 +38,41 @@ export default function Home() {
         opacity: 0,
         y: 10,
         duration: 0.6,
+      });
+
+    const scrollTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+
+    scrollTL
+      .fromTo(
+        "body",
+        {
+          backgroundColor: "#FDE047",
+        },
+        {
+          backgroundColor: "#D9F99D",
+          overwrite: "auto",
+        },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", {
+        y: 20,
+        opacity: 0,
       });
   });
 
@@ -81,9 +118,9 @@ export default function Home() {
                 </h2>
                 <div className="text-side-body mt-4 max-w-xl text-balance text-xl font-normal text-sky-950">
                   <TextSplitter
-                    text={`Our soda is made with real fruit and a rouch of cane sugar. We
+                    text="Our soda is made with real fruit and a rouch of cane sugar. We
                   never use artificial sweeteners or high fructose corn syrup.
-                  Try all ive flavors and find your favorite!`}
+                  Try all ive flavors and find your favorite!"
                   />
                 </div>
               </div>
