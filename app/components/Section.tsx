46269bcb0a5ed2e4b96c45b8f1130ce8c9f69cff
Almira Dyan Gtierrez <gutierrez.almiradyan@gmail.com>
2025-07-01 18:15:56 +0800

    fix: left 3D can model being cit off from the left side

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -6,7 +6,9 @@ type Props = { className?: string; children: ReactNode };
 export default function Section({ className, children }: Props) {
   return (
     <div className={clsx("px-4 first:pt-10 md:px-6", className)}>
-      {children}
+      <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
+        {children}
+      </div>
     </div>
   );
 }
